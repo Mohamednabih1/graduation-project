1585c6ee51ddb33cb8169c31f851890fdfcef3c6
Ahme10D6 <ahmed96bob@gmail.com>
2024-03-06 20:07:35 +0200

    added exercise page

---
+++ lib/presentation/ui/exercise_page/view_model/exercise_view_model.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: unused_import
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gradproject/app/constants/routes_constants.dart';
+import 'package:gradproject/app/di.dart';
+import 'package:gradproject/data/data_source/local_data_source.dart/permanent_data_source/shared_preferences.dart';
+import 'package:gradproject/presentation/ui/common/base/base_view_model.dart';
+
+class ExerciseViewModel extends BaseViewModel with ChangeNotifier {
+  void logOut(BuildContext context)
+  {
+
+  }
+  void onItemTapped(int index) {
+
+  }
+@override
+  void start()
+  {
+
+  }
+}
